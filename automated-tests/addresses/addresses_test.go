c4f6772f92f7ce95e0518f082c6ad9f06bae2c74
Diego Pimenta <diegopimenta888@gmail.com>
2025-05-12 15:03:15 -0300

    test: go cmds for tests and coverage stats

--- automated-tests/addresses/addresses_test.go
+++ automated-tests/addresses/addresses_test.go
@@ -1,14 +1,30 @@
 // it's mandatory to tests follow this the naming convention: classname_test.go
-package addresses
+package addresses_test
 
-import "testing"
+import (
+	. "automated-tests/addresses" // create an alias for the package
+	"testing"
+)
 
 type testScenario struct {
 	testAddress  string
 	expectedType string
 }
 
+/*
+"go test" runs the tests in the current package
+"go test ./..." runs all tests in the current directory and subdirectories
+
+flag "-v" shows the details of the tests
+flag "--cover" shows the coverage of the tests
+
+"go test --coverprofile coverage.txt" creates a coverage file
+"go tool cover --html=coverage.txt" shows the coverage in a browser
+*/
+
 func TestAddressType(t *testing.T) { // parameter has to be of type *testing.T
+	t.Parallel() // tests with this function will run in parallel
+
 	scenarios := []testScenario{
 		{"Avenida Santos Lopes", "Avenida"},
 		{"Rodovia Bela Castro", "Rodovia"},
